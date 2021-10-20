e8b8740922f35d7f5a4a46c26e34160289733802
Douglas Henrique Alves <doughalves@gmail.com>
2021-10-20 12:04:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -54,8 +54,252 @@ cartas = [
     img: "./imagens/personagens/009blastoise.jpg",
     moldura: "./imagens/cartas/water.png"
   },
-  
-  
+  {
+    Nome: "Caterpie",
+    Atributos: {Ataque: 30, Defesa: 35, Velocidade: 45},
+    img: "./imagens/personagens/010caterpie.jpg",
+    moldura: "./imagens/cartas/bug.png"
+  },
+  {
+    Nome: "Metapod",
+    Atributos: {Ataque: 20, Defesa: 55, Velocidade: 30},
+    img: "./imagens/personagens/011metapod.jpg",
+    moldura: "./imagens/cartas/bug.png"
+  },
+  {
+    Nome: "Butterfree",
+    Atributos: {Ataque: 45, Defesa: 50, Velocidade: 70},
+    img: "./imagens/personagens/012butterfree.jpg",
+    moldura: "./imagens/cartas/bug.png"
+  },
+  {
+    Nome: "Weedle",
+    Atributos: {Ataque: 35, Defesa: 30, Velocidade: 50},
+    img: "./imagens/personagens/013weedle.jpg",
+    moldura: "./imagens/cartas/bug.png"
+  },
+  {
+    Nome: "Kakuna",
+    Atributos: {Ataque: 25, Defesa: 50, Velocidade: 35},
+    img: "./imagens/personagens/014kakuna.jpg",
+    moldura: "./imagens/cartas/bug.png"
+  },
+  {
+    Nome: "Beedrill",
+    Atributos: {Ataque: 90, Defesa: 40, Velocidade: 75},
+    img: "./imagens/personagens/015beedrill.jpg",
+    moldura: "./imagens/cartas/bug.png"
+  },
+  {
+    Nome: "Pidgey",
+    Atributos: {Ataque: 45, Defesa: 40, Velocidade: 56},
+    img: "./imagens/personagens/016pidgey.jpg",
+    moldura: "./imagens/cartas/flying.png"
+  },
+  {
+    Nome: "Pidgeotto",
+    Atributos: {Ataque: 60, Defesa: 55, Velocidade: 71},
+    img: "./imagens/personagens/017pidgeotto.jpg",
+    moldura: "./imagens/cartas/flying.png"
+  },
+  {
+    Nome: "Pidgeot",
+    Atributos: {Ataque: 80, Defesa: 75, Velocidade: 101},
+    img: "./imagens/personagens/018pidgeot.jpg",
+    moldura: "./imagens/cartas/flying.png"
+  },
+  {
+    Nome: "Rattata",
+    Atributos: {Ataque: 56, Defesa: 35, Velocidade: 72},
+    img: "./imagens/personagens/019rattata.jpg",
+    moldura: "./imagens/cartas/normal.png"
+  },
+  {
+    Nome: "Raticate",
+    Atributos: {Ataque: 81, Defesa: 60, Velocidade: 97},
+    img: "./imagens/personagens/020raticate.jpg",
+    moldura: "./imagens/cartas/normal.png"
+  },
+  {
+    Nome: "Spearow",
+    Atributos: {Ataque: 60, Defesa: 30, Velocidade: 70},
+    img: "./imagens/personagens/021spearow.jpg",
+    moldura: "./imagens/cartas/flying.png"
+  },
+  {
+    Nome: "Fearow",
+    Atributos: {Ataque: 90, Defesa: 65, Velocidade: 100},
+    img: "./imagens/personagens/022fearow.jpg",
+    moldura: "./imagens/cartas/flying.png"
+  },
+  {
+    Nome: "Ekans",
+    Atributos: {Ataque: 60, Defesa: 44, Velocidade: 55},
+    img: "./imagens/personagens/023ekans.jpg",
+    moldura: "./imagens/cartas/poison.png"
+  },
+  {
+    Nome: "Arbok",
+    Atributos: {Ataque: 95, Defesa: 69, Velocidade: 80},
+    img: "./imagens/personagens/024arbok.jpg",
+    moldura: "./imagens/cartas/poison.png"
+  },
+  {
+    Nome: "Pikachu",
+    Atributos: {Ataque: 55, Defesa: 40, Velocidade: 90},
+    img: "./imagens/personagens/025pikachu.jpg",
+    moldura: "./imagens/cartas/eletric.png"
+  },
+  {
+    Nome: "Raichu",
+    Atributos: {Ataque: 90, Defesa: 55, Velocidade: 110},
+    img: "./imagens/personagens/026raichu.jpg",
+    moldura: "./imagens/cartas/eletric.png"
+  },
+  {
+    Nome: "Sandshrew",
+    Atributos: {Ataque: 75, Defesa: 85, Velocidade: 40},
+    img: "./imagens/personagens/027sandshrew.jpg",
+    moldura: "./imagens/cartas/ground.png"
+  },
+  {
+    Nome: "Sandslash",
+    Atributos: {Ataque: 100, Defesa: 110, Velocidade: 65},
+    img: "./imagens/personagens/028sandslash.jpg",
+    moldura: "./imagens/cartas/ground.png"
+  },
+  {
+    Nome: "Nidoran ♀",
+    Atributos: {Ataque: 47, Defesa: 52, Velocidade: 41},
+    img: "./imagens/personagens/029nidoran-f.jpg",
+    moldura: "./imagens/cartas/poison.png"
+  },
+  {
+    Nome: "Nidorina",
+    Atributos: {Ataque: 62, Defesa: 67, Velocidade: 56},
+    img: "./imagens/personagens/030nidorina.jpg",
+    moldura: "./imagens/cartas/poison.png"
+  },
+  {
+    Nome: "Nidoqueen",
+    Atributos: {Ataque: 92, Defesa: 87, Velocidade: 76},
+    img: "./imagens/personagens/031nidoqueen.jpg",
+    moldura: "./imagens/cartas/poison.png"
+  },
+  {
+    Nome: "Nidoran ♂",
+    Atributos: {Ataque: 57, Defesa: 40, Velocidade: 50},
+    img: "./imagens/personagens/032nidoran-m.jpg",
+    moldura: "./imagens/cartas/poison.png"
+  },
+  {
+    Nome: "Nidorino",
+    Atributos: {Ataque: 72, Defesa: 57, Velocidade: 65},
+    img: "./imagens/personagens/033nidorino.jpg",
+    moldura: "./imagens/cartas/poison.png"
+  },
+  {
+    Nome: "Nidoking",
+    Atributos: {Ataque: 102, Defesa: 77, Velocidade: 85},
+    img: "./imagens/personagens/034nidoking.jpg",
+    moldura: "./imagens/cartas/poison.png"
+  },
+  {
+    Nome: "Clefairy",
+    Atributos: {Ataque: 45, Defesa: 48, Velocidade: 35},
+    img: "./imagens/personagens/035clefairy.jpg",
+    moldura: "./imagens/cartas/fairy.png"
+  },
+  {
+    Nome: "Clefable",
+    Atributos: {Ataque: 70, Defesa: 73, Velocidade: 60},
+    img: "./imagens/personagens/036clefable.jpg",
+    moldura: "./imagens/cartas/fairy.png"
+  },
+  {
+    Nome: "Vulpix",
+    Atributos: {Ataque: 41, Defesa: 40, Velocidade: 65},
+    img: "./imagens/personagens/037vulpix.jpg",
+    moldura: "./imagens/cartas/fire.png"
+  },
+  {
+    Nome: "Ninetales",
+    Atributos: {Ataque: 76, Defesa: 75, Velocidade: 100},
+    img: "./imagens/personagens/038ninetales.jpg",
+    moldura: "./imagens/cartas/fire.png"
+  },
+  {
+    Nome: "Jigglypuff",
+    Atributos: {Ataque: 45, Defesa: 20, Velocidade: 20},
+    img: "./imagens/personagens/039jigglypuff.jpg",
+    moldura: "./imagens/cartas/fairy.png"
+  },
+  {
+    Nome: "Wigglytuff",
+    Atributos: {Ataque: 70, Defesa: 45, Velocidade: 45},
+    img: "./imagens/personagens/040wigglytuff.jpg",
+    moldura: "./imagens/cartas/fairy.png"
+  },
+  {
+    Nome: "Zubat",
+    Atributos: {Ataque: 45, Defesa: 35, Velocidade: 55},
+    img: "./imagens/personagens/041zubat.jpg",
+    moldura: "./imagens/cartas/poison.png"
+  },
+  {
+    Nome: "Golbat",
+    Atributos: {Ataque: 80, Defesa: 70, Velocidade: 90},
+    img: "./imagens/personagens/042golbat.jpg",
+    moldura: "./imagens/cartas/poison.png"
+  },
+  {
+    Nome: "Oddish",
+    Atributos: {Ataque: 50, Defesa: 55, Velocidade: 30},
+    img: "./imagens/personagens/043oddish.jpg",
+    moldura: "./imagens/cartas/grass.png"
+  },
+  {
+    Nome: "Gloom",
+    Atributos: {Ataque: 65, Defesa: 70, Velocidade: 40},
+    img: "./imagens/personagens/044gloom.jpg",
+    moldura: "./imagens/cartas/grass.png"
+  },
+  {
+    Nome: "Vileplume",
+    Atributos: {Ataque: 80, Defesa: 85, Velocidade: 50},
+    img: "./imagens/personagens/045vileplume.jpg",
+    moldura: "./imagens/cartas/grass.png"
+  },
+  {
+    Nome: "Paras",
+    Atributos: {Ataque: 70, Defesa: 55, Velocidade: 25},
+    img: "./imagens/personagens/046paras.jpg",
+    moldura: "./imagens/cartas/bug.png"
+  },
+  {
+    Nome: "Parasect",
+    Atributos: {Ataque: 95, Defesa: 80, Velocidade: 30},
+    img: "./imagens/personagens/047parasect.jpg",
+    moldura: "./imagens/cartas/bug.png"
+  },
+  {
+    Nome: "Venonat",
+    Atributos: {Ataque: 55, Defesa: 50, Velocidade: 45},
+    img: "./imagens/personagens/048venonat.jpg",
+    moldura: "./imagens/cartas/bug.png"
+  },
+  {
+    Nome: "Venomoth",
+    Atributos: {Ataque: 65, Defesa: 60, Velocidade: 90},
+    img: "./imagens/personagens/049venomoth.jpg",
+    moldura: "./imagens/cartas/bug.png"
+  },
+  {
+    Nome: "Diglett",
+    Atributos: {Ataque: 55, Defesa: 25, Velocidade: 95},
+    img: "./imagens/personagens/050diglett.jpg",
+    moldura: "./imagens/cartas/ground.png"
+  },
 ];
 
 // Onde serão salvas as cartas sorteadas
@@ -88,7 +332,7 @@ function zerarCarta(id) {
   var opcoes = document.getElementById(id);
   // Mostra o verso da carta
   var opcoesTexto =
-    '<img src="https://guloseimasnerds.files.wordpress.com/2017/02/verso-carta-pkm.jpg" style=" width: inherit; height: inherit; position: absolute;">';
+    '<img src="./imagens/cartas/verso-carta-pkm.jpg" style=" width: inherit; height: inherit; position: absolute;">';
   opcoes.innerHTML = opcoesTexto;
 }
 
